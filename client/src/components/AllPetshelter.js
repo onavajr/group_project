3581fab0c93115002dc820f4df30aadd4174f835
orlando_n <galunggong14@gmail.com>
2021-06-22 15:34:49 -0700

    created fron end files

---
+++ client/src/components/AllPetshelter.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState } from 'react'
+import axios from 'axios';
+import {Link, navigate} from '@reach/router';
+
+const AllPetshelter = (props) => {
+    return (
+        <div>
+            <h1>All Pets</h1>
+        </div>
+    )
+}
+
+export default AllPetshelter;
---
+++ client/src/components/CreatePetshelter.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState } from 'react'
+import axios from 'axios';
+import {Link, navigate} from '@reach/router';
+
+const CreatePetShelter = (props) => {
+    return (
+        <div>
+            <h1>Create Pets</h1>
+        </div>
+    )
+}
+
+export default CreatePetShelter;
