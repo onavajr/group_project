1a7311a87f82b8144287336b13bbaf813241a884
orlando_n <galunggong14@gmail.com>
2021-06-22 18:01:28 -0700

    All pets has details and can be edit

--- client/src/components/AllPetshelter.js
+++ client/src/components/AllPetshelter.js
@@ -3,9 +3,37 @@ import axios from 'axios';
 import {Link, navigate} from '@reach/router';
 
 const AllPetshelter = (props) => {
+    const [ allPets, setAllPets] = useState([]);
+    useEffect(() =>{
+        axios.get('http://localhost:8000/api/petshelter')
+            .then((res)=>{
+                console.log(res.data);
+                setAllPets(res.data);
+            })
+            .catch((err)=>console.log(err));
+
+    }, []);
     return (
-        <div>
-            <h1>All Pets</h1>
+        <div className="pet">
+            <div>
+                <h1>Pet Shelter</h1>
+                <p>Add a pet to the Shelter</p>
+            </div>
+            <h3>These pets are looking for a good home</h3>
+                <div>
+                    {
+                        allPets.map(( pet, index) =>(
+                            <div key={ index }>
+                                <p>{ pet.petname }</p>
+                                <p>{ pet.pettype }</p>
+                                <div>
+                                    <p><Link to = {"/petshelter/" + pet._id}>Details</Link></p> |
+                                    <p>Edit</p>
+                                </div>
+                            </div>
+                            ))
+                        }
+                </div>
         </div>
     )
 }
