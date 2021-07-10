2200929d9a8a9153e2ab5b0fd3fe667b6e9ee019
orlando_n <galunggong14@gmail.com>
2021-07-10 09:46:51 -0700

    Sign in page done

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 
 function Layout({children}) {
     return (
-        <div>
+        <div className="container">
             <NavBar />
             {children}
         </div>
