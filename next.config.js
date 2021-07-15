aa109aa865f5efd3e19b6e9c9a0afde30802d046
orlando_n <galunggong14@gmail.com>
2021-07-14 21:44:24 -0700

    Adding new features

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-      "BASE_URL": "http://localhost:3000",
-      "MONGODB_URL": "mongodb+srv://galunggong18:Ogagako_18@cluster0.o4tmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  }
-}
